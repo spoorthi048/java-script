1180257005655fe27dc20eba4e4e8cc0351989c4
spoorthi048 <spoorthi.23ad048@sode-edu.in>
2025-10-02 15:32:43 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score="Spoorthi"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(valueInNumber);
+console.log(typeof(valueInNumber));
+
+
+// "33"=> 33
+// "33abc"=>NaN
+//true=> 1; false=>0
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+
+//1=>true
+//0=>false
+// ""=>false
+// "hitesh"=>true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
